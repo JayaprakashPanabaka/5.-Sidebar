d65ffa04c999b1c53f17977988311c5b583612b6
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2024-02-10 11:40:27 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let toggleBtn = document.querySelector('.sidebar-toggle')
+let closeBtn = document.querySelector('.close-btn')
+let sidebar = document.querySelector('.sidebar')
+
+toggleBtn.addEventListener("click", () => {
+    // if(sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove('show-sidebar')
+    // } else {
+    //     sidebar.classList.add('show-sidebar')
+    // }
+    sidebar.classList.toggle("show-sidebar")
+})
+
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove('show-sidebar')
+})
+
+
